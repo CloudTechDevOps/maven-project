dfc8460d553255999d55ed9f912550065b4c1327
CloudTechDevOps <147053314+CloudTechDevOps@users.noreply.github.com>
2025-07-31 08:03:30 +0530

    Update AppTest.java

--- src/test/java/com/project/AppTest.java
+++ src/test/java/com/project/AppTest.java
@@ -8,17 +8,17 @@ public class AppTest {
     @Test
     public void testLogin1() {
         App myApp = new App();
-        Assert.assertEquals(1, myApp.userLogin("abc", "abc123"));
+        Assert.assertEquals(0, myApp.userLogin("abc", "abc123"));
     }
 
     @Test
     public void testLogin2() {
         App myApp = new App();
-        Assert.assertEquals(0, myApp.userLogin("abc", "abc@123"));
+        Assert.assertEquals(1, myApp.userLogin("abc", "abc@123"));
     }
      @Test
     public void testLogin3() {
         App myApp = new App();
-        Assert.assertEquals(1, myApp.userLogin("abc", "abc@1234"));
+        Assert.assertEquals(0, myApp.userLogin("abc", "abc@1234"));
     }
 }
